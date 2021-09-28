f65923e1430f502be05e45e5ad338db7f2355491
Diogo Magliano <diogo.magliano@zup.com.br>
2021-09-28 15:40:43 -0300

    Implementando o RestControllerAdvice e error handler que ficou faltando.

---
+++ src/main/java/br/com/zupacademy/dmagliano/casadocodigo/controller/dto/FieldErrorOutputDto.java
@@ -0,0 +1,20 @@
+package br.com.zupacademy.dmagliano.casadocodigo.controller.dto;
+
+public class FieldErrorOutputDto {
+
+    private String field;
+    private String message;
+
+    public FieldErrorOutputDto(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/br/com/zupacademy/dmagliano/casadocodigo/controller/validator/ProibeEmailDuplicadoAutorValidator.java
@@ -0,0 +1,37 @@
+package br.com.zupacademy.dmagliano.casadocodigo.controller.validator;
+
+import br.com.zupacademy.dmagliano.casadocodigo.controller.dto.AutorForm;
+import br.com.zupacademy.dmagliano.casadocodigo.model.Autor;
+import br.com.zupacademy.dmagliano.casadocodigo.repository.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Optional;
+
+@Component
+public class ProibeEmailDuplicadoAutorValidator implements Validator {
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return AutorForm.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        if (errors.hasErrors()) {
+            return;
+        }
+        AutorForm form = (AutorForm) target;
+        Optional<Autor> autorExistente = autorRepository.findByEmail(form.getEmail());
+        if (autorExistente.isPresent()) {
+            errors.rejectValue("email", null,
+                    "Já existe autor/a o email cadastrado: " + form.getEmail());
+        }
+
+    }
+}
